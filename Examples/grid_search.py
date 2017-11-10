74b4094830ef2d9f24911fda32ee73a3ac33aa9d
amloren1 <amloren1@asu.edu>
2017-11-09 18:49:08 -0700

    updated grid searcher

--- Examples/grid_search.py
+++ Examples/grid_search.py
@@ -2,7 +2,7 @@
 
 #**********************************************************************#
 '''
-This script enables the user to input a simplified composition 
+This script enables the user to input a simplified composition
 Fe/Mg and Si/Mg, and a mass to find the corresponding radius, CMF, CRF
 '''
 #*********************************************************************
@@ -23,6 +23,12 @@ import PREM.prem as p
 from params import *
 import pdb
 
+#mass grid for interpolation
+Mass = np.arange(0.5, 1.8,0.1)
+
+Rads = np.zeros(len(Mass))
+
+
 
 
 ################
@@ -35,18 +41,14 @@ Steps:
 3. create routine to find the radii which correspond to input composition
 '''
 ################
-def get_cmap(n, name='hsv'):
-    '''Returns a function that maps each index in 0, 1, ..., n-1 to a distinct 
-    RGB color; the keyword argument name must be a standard mpl colormap name.'''
-    return plt.cm.get_cmap(name, n)
 
 def bissect(grid, value):
-    
+
     mid = int(len(grid)/2.)
     hi  = int(len(grid)-1)
     lo  = 0
     if value/max(grid) > 1.0+1e-6 :
-        
+
         print 'Exiting. The following value is not in the grids yet:'
         print '{}'.format(value)
         print 'Max = {}'.format(max(grid))
@@ -68,9 +70,9 @@ def interpolant(lo, hi, val):
     return q
 
 def interpd_data(q, y1, y2):
-    
+
     y = y1+q*(y2-y1)
-    
+
     return y
 
 
@@ -82,10 +84,11 @@ def interpd_data(q, y1, y2):
 FeMg = np.arange(0.7, 1.7, 0.1)
 SiMg = np.arange(0.7, 1.7, 0.1)
 
-def loc_data(FeMg_grid, SiMg_grid, femg, simg):
-    
-    femg_i = bissect(FeMg_grid, femg)[0]
-    simg_i = bissect(SiMg_grid, simg)[0]
+def loc_data(FeMg_grid, SiMg_grid, h2o_grid, femg, simg, wr_h2o):
+
+    femg_i   = bissect(FeMg_grid, femg)[0]
+    simg_i   = bissect(SiMg_grid, simg)[0]
+    wt_h2o_i = bissect(h2o_grid, wr_h2o)
     #loc is the row in any mass file which is the specified composition
     loc    = femg_i*len(FeMg_grid)+simg_i
 
@@ -96,7 +99,7 @@ def loc_data(FeMg_grid, SiMg_grid, femg, simg):
 def file_names(mass):
     mass_string = repr(round(mass,2)).replace('.', ',')
     name = mass_string+'.dat'
-    
+
     return name
 
 ########################################################################
@@ -105,15 +108,11 @@ Import all of the mass files
 '''
 ########################################################################
 
-#mass grid for interpolation
-Mass = np.arange(0.5, 1.8,0.1)
-
-Rads = np.zeros(len(Mass))
-
 
 ####
 #Pull data from arrays for a specific composition
 ####
+
 def make_data_arrays(femg, simg):
 
     N_mas = len(Mass)
@@ -127,33 +126,34 @@ def make_data_arrays(femg, simg):
     #pull data row from each file
     for i in range(len(Mass)):
         f_name = file_names(Mass[i])
-        #'Mass', 'Radius','Fe/Mg','Si/Mg', 'Bulk density', 'core_wt%', 'core_rad%'
+        #'Mass', 'Radius','Fe/Mg','Si/Mg', 'wt_h2o', 'Bulk density', 'core_wt%', 'core_rad%'
         dat = np.genfromtxt(f_name)
-    
+
         dat_row = dat[row,:]
-    
-        Rads[i]    = dat_row[1] 
+
+        Rads[i]    = dat_row[1]
         t_femg[i]  = dat_row[2]
         t_simg[i]  = dat_row[3]
-        rho[i]     = dat_row[4]
-        cmf[i]     = dat_row[5]
-        crf[i]     = dat_row[6]
-        
+        t_wth2o[i]  = dat_row[4]
+        rho[i]     = dat_row[5]
+        cmf[i]     = dat_row[6]
+        crf[i]     = dat_row[7]
+
     keys = ['Mass', 'Radius','FeMg','SiMg', 'rho', 'CMF', 'CRF']
-    
+
     return(dict(zip(keys,[Mass, Rads, t_femg, t_simg, rho, cmf, crf])))
-   
+
 ####
 # plot limits: pure fe, pure mantle and pure water/ice
-#### 
+####
 
 def plot_limits(f_name):
-    
+
     dat = np.genfromtxt(f_name)
     mas = dat[:,0]
     rads = dat[:,1]
 
-    ax.plot(mas, rads, lw = 5, color = 'dimgray', alpha = 0.8)   
+    ax.plot(mas, rads, lw = 5, color = 'dimgray', alpha = 0.8)
     ax.annotate('100% Fe', xy=(np.median(mas), np.median(rads)*0.978), \
      xytext = (np.median(mas), np.median(rads)*0.978), fontsize = 16, \
      rotation=10, weight = 'bold')
@@ -161,7 +161,7 @@ def plot_limits(f_name):
 ####
 # take composition, find data arrays, interpolate to find radius
 # plot on MvR
-#### 
+####
 def plot_comps(femg, simg, mass, err):
     import random
     r = lambda: random.randint(0,255)
@@ -173,19 +173,19 @@ def plot_comps(femg, simg, mass, err):
         rho = dat.get('rho')
         cmf = dat.get('cmf')
         crf = dat.get('crf')
-        
+
         #find interpolation value
         mid, lo, hi = bissect(Mass, mass)
-        
+
         q = interpolant(Mass[lo],Mass[hi],mass)
-     
+
         radius = interpd_data(q, Rads[lo], Rads[hi])
-        
+
         return radius, Rads
-    
-    
+
+
     plot_limits('pure_Fe.dat')
-    
+
     lab_size = 23
     tic_size = 18
 
@@ -196,7 +196,7 @@ def plot_comps(femg, simg, mass, err):
     ax.set_xlim(min(Mass)*.9, max(Mass)*1.06)
     ax.set_ylim(0.5, 1.28)
     for i in range(len(femg)):
-        
+
         radius, rads = find_radius(femg[i], simg[i], mass[i])
         no_lab = "_nolegend_"
         l1 = 'Fe/Mg = {}; Si/Mg = {}'.format(femg[i],simg[i])
@@ -204,7 +204,7 @@ def plot_comps(femg, simg, mass, err):
             l2 = 'Fe/Mg = {}; Si/Mg = {}'.format(femg[i+1],simg[i+1])
         else:
             l2 = 'dummd'
-        
+
         j = ('#%02X%02X%02X' % (r(),r(),r()))
         ax.scatter(mass[i],radius, marker = 'H', linewidth = 7, c = j, edgecolor=j,  \
                   label = 'R(M={})={}'.format(mass[i], radius))
@@ -215,9 +215,9 @@ def plot_comps(femg, simg, mass, err):
         if l1 != l2:
             ax.plot(Mass, rads, label = l1, lw = 5, alpha = 0.8)
         plt.draw()
-    
-    
-    
+
+
+
     plt.legend(loc = 'lower right', fontsize = tic_size, scatterpoints=1)
     plt.show()
 
@@ -240,9 +240,9 @@ Q = interpolant(Mass[lo], Mass[high], test)
 print '\n{} is at location = {} '.format(test, mid)
 print 'mid = {} \nlow = {} \nhi = {}\n'.format(mid, lo, high)
 print 'interpolant between M_lo = {} and M_hi = {} \nis q = {}'.format(Mass[lo], \
-                                Mass[high],Q) 
+                                Mass[high],Q)
 
-print Mass  
+print Mass
 
 
 
