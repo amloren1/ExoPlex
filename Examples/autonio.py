a2cc532f1a7316cb70f1997a16cbd594841d9711
amloren1 <alexlorenzo39@gmail.com>
2017-11-13 16:36:20 -0700

    updated perplex auto-runner

--- Examples/autonio.py
+++ Examples/autonio.py
@@ -1,3 +1,40 @@
+# coding: utf-8
+# This file is part of ExoPlex - a self consistent planet builder
+# Copyright (C) 2017 - by the ExoPlex team, released under the GNU
+# GPL v2 or later.
+
+
+#**********************************************************************#
+'''
+This module is intended to run Perple_X through a parameter space to
+create corresponding phase diagrams for each composition. Perple_X is
+computationally expensive and very time consuming so creating these
+mineral data files for a range of compositions before running ExoPlex
+is a highly recommended.
+
+To use this module properly, the user must input their compositions with
+the frame of mind that they are defining a bulk planet composition. The
+inputs are convolved and converted into stoichiometric inputs for perple_x.
+There are two ways to do this:
+
+1) The user may input bulk planet composition
+    - key parameters:
+        define_mantle = False
+    -keep in mind all composition inputs are global aso the core and
+    mantle are convolved
+2) Define mantle composition directly
+    - key parameters:
+        define_mantle = {'fix_man': True, 'wtCore': .50}
+        fFeO = 0.0  *must be zero because we are directly handling mantle composition
+
+
+Beyond composition, use the default parameters which are located in params.py
+
+'''
+#**********************************************************************#
+
+
+
 import os
 import sys
 import numpy as np
@@ -9,81 +46,66 @@ if not os.path.exists('ExoPlex') and os.path.exists('../ExoPlex'):
 
 import ExoPlex as exo
 
-run = False
-MEarth = 5.972e24 #kg
-REarth = 6.371e6   #meters
+
+from params import *
 
 
+######
+'''
+Bulk elemental abundances input as lists or arrays (e.g. FeMg = (Fe/Mg)_mol)
 
+'''
+######
 
+define_mantle = 0.0 #or dictionary to define mantle comp directly: = {'fix_man': True, 'wtCore': .50}
 
-Star = 'Rando'
-CaMg = 0.0 #0.0616595 #
-AlMg = 0.0 #0.0851138
+CaMg = [0.0, 0.06]
+AlMg = [0.0, 0.08]
 FeMg = [0.5,.7,.8,.9,1.0,1.1,1.2,1.3,1.4,1.5,1.6,1.7, 1.8, 1.9, 2.0]
 SiMg = [0.5,.7,.8,.9,1.0,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8, 1.9, 2.0]
 
+####
+'''
+global fraction of Fe moles in the mantle. Essentially a global Fe redox
+state parameter.
+'''
+####
 
-# among light elements, vary only the silicon value
+fFeO = 0.0
+
+######
+'''
+Core composition of Si, O, and S input as core mass fraction. 
+These have no impact if you are defining the mantle directly. 
+Otherwise, they will impact the stoichiometry of the mantle.
+'''
+######
+
 wt_frac_Si_core = 0. #0 to 30wt% Si in core
 wt_frac_O_core  = 0.
 wt_frac_S_core  = 0.
 
-#h2o content 0 to 1.0
-wt_frac_water = 0.0
-
-#how much Fe in the mantle by mole. This is Fe oxidation state
-XFeO = 0.0
-
-#(Perplex) P&T parameter space definitions for perplex
-#UM-upper mantle, LM-lower mantle
-Pressure_range_mantle_UM    = '3000 1400000'
-Temperature_range_mantle_UM = '1400 3000'
-resolution_UM               = '60 60'
-
-Pressure_range_mantle_LM    = '1250000 6500000'
-Temperature_range_mantle_LM = '2500 5000'
-resolution_LM               = '50 50'
 
 
-#layers, like concentric shells set here in each region: core, mantle, h20 envelope
-num_mantle_layers = 400
-num_core_layers   = 200
-number_h2o_layers = 0
+# lists of compositional and structural inputs used to build planet
 
-#temperature at surface if no water layer. Essentially temperature below the crust
-Mantle_potential_temp = 1700.
+structure_params = [Pressure_range_mantle_UM, Temperature_range_mantle_UM, resolution_UM,
+                    Pressure_range_mantle_LM, Temperature_range_mantle_LM, resolution_LM,
+                    Core_rad_frac_guess, Mantle_potential_temp, h20_radfrac_guess, T_surface_h2o]
 
-#h2o potential Temp, surface temperature if there exists an h2o layer
-T_surface_h2o = 300. # K
-
-#initialize planet with these guesses for radial fraction of core and water layer
-Core_rad_frac_guess = .54
-h20_radfrac_guess   = 0.1
-
-
-structure_params =  [Pressure_range_mantle_UM,Temperature_range_mantle_UM,resolution_UM,
-                     Pressure_range_mantle_LM, Temperature_range_mantle_LM, resolution_LM,
-                     Core_rad_frac_guess,Mantle_potential_temp, h20_radfrac_guess, T_surface_h2o]
-
-
-
-
-
-
-
-
-
-layers = [num_mantle_layers,num_core_layers,0]
+layers = [num_mantle_layers, num_core_layers, number_h2o_layers]
 
 Mass = 1.0
-for q in range(len(FeMg)):
-    for p in range(len(SiMg)):
+for f in range(len(FeMg)):
+    for s in range(len(SiMg)):
+        for c in range(len(CaMg)):
+            for a in range(len(AlMg)):
 
-        compositional_params = [wt_frac_water, FeMg[q], SiMg[p], CaMg, AlMg, XFeO, wt_frac_Si_core, \
-                                wt_frac_O_core, wt_frac_S_core]
-#run routines to build perplex solution and planet
-        Planet = exo.run_planet_mass(Mass, compositional_params,structure_params,layers,'blank', False)
+                compositional_params = [wt_frac_water, FeMg[f], SiMg[s], CaMg[c], AlMg[a], \
+                                        fFeO, wt_frac_Si_core, \
+                                        wt_frac_O_core, wt_frac_S_core]
+                #run routines to build perplex solution
+                Planet = exo.run_perplex_only(compositional_params,structure_params,layers,'planet', define_mantle)
 
 
 
