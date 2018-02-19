0f3dd2b07be29cf9ba9fe1ae10fba83b3bf4dfcf
vm <amloren1@asu.edu>
2018-02-19 13:54:29 -0700

    added new directory to store scripts which user will interact with

---
+++ run/models.py
@@ -0,0 +1,30 @@
+# coding: utf-8
+# This file is part of ExoPlex - a self consistent planet builder
+# Copyright (C) 2017 - by the ExoPlex team, released under the GNU
+# GPL v2 or later.
+
+
+#**********************************************************************#
+'''
+This script contains functions which call on the ExoPlex backend code
+to create planets in a veriety of scenarios:
+-Model earth
+-Prem
+-single models of fixed radius or mass
+    - calculate core mass fraction or leave it open
+
+'''
+#**********************************************************************#
+
+import os
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+# hack to allow scripts to be placed in subdirectories next to burnman:
+if not os.path.exists('ExoPlex') and os.path.exists('../ExoPlex'):
+    sys.path.insert(1, os.path.abspath('..'))
+
+
+import ExoPlex as exo
+import PREM.prem as p
+from params import *
